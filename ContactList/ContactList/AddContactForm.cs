72e8020e3ce9da68679ecb8a98fd5eb019cdba7b
Zangestsru <tsmdoubleliff@gmail.com>
2025-05-28 09:12:19 +0700

    2280602523-ngogiaphuong-them form de them lien he moi

--- ContactList/ContactList/AddContactForm.cs
+++ ContactList/ContactList/AddContactForm.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace ContactList
 {
-    public partial class Form1 : Form
+    public partial class AddContactForm : Form
     {
-        public Form1()
+        public AddContactForm()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
