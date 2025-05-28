c61344f04109da1243f2baf8daafb35a4a0db98c
Lê Ngọc Linh <ngoclinhh1202@gmail.com>
2025-05-28 08:57:38 +0700

    2280601717-LeNgocLinh-Khoi tao du an window form voi mainform

--- ContactList/ContactList/Form1.Designer.cs
+++ ContactList/ContactList/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(211, 113);
+            this.label1.Location = new System.Drawing.Point(90, 118);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 16);
             this.label1.TabIndex = 0;
@@ -43,7 +43,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(256, 250);
+            this.button1.Location = new System.Drawing.Point(290, 260);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(205, 23);
             this.button1.TabIndex = 1;
